cd677603ac75467bfad978b31b99a8e3fc44957c
SWASTIKA-SINGHA <swastikas857@gmail.com>
2025-07-21 14:27:10 +0530

    Update project_py.py

--- project_py.py
+++ project_py.py
@@ -1,14 +1,9 @@
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 
-# Load the iris dataset
-iris = load_iris()
-X = iris.data
-y = iris.target
-clf = RandomForestClassifier()
-clf.fit(X, y)
-
 def predict_iris(sepal_length, sepal_width, petal_length, petal_width):
-    input_data = [[sepal_length, sepal_width, petal_length, petal_width]]
-    prediction = clf.predict(input_data)
+    iris = load_iris()
+    clf = RandomForestClassifier()
+    clf.fit(iris.data, iris.target)
+    prediction = clf.predict([[sepal_length, sepal_width, petal_length, petal_width]])
     return iris.target_names[prediction[0]]
