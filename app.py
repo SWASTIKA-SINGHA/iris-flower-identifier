424afd76b12aa887f27d498a536dc8822729b52d
SWASTIKA-SINGHA <swastikas857@gmail.com>
2025-07-21 14:19:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ st.set_page_config(page_title="Iris Flower Identifier 🌸", layout="centered")
 st.title("🌸 Iris Flower Identifier")
 st.write("Enter the flower's measurements below to predict its species:")
 
-# Input fields
 sepal_length = st.number_input("Sepal Length (cm)", min_value=0.0, format="%.2f")
 sepal_width = st.number_input("Sepal Width (cm)", min_value=0.0, format="%.2f")
 petal_length = st.number_input("Petal Length (cm)", min_value=0.0, format="%.2f")
