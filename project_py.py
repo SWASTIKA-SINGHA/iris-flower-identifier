468eafcd73e65ce50dad6cfa7e4e6e9d0e6fd604
SWASTIKA-SINGHA <swastikas857@gmail.com>
2025-07-21 15:00:45 +0530

    Update project_py.py

--- project_py.py
+++ project_py.py
@@ -7,3 +7,8 @@ def predict_iris(sepal_length, sepal_width, petal_length, petal_width):
     clf.fit(iris.data, iris.target)
     prediction = clf.predict([[sepal_length, sepal_width, petal_length, petal_width]])
     return iris.target_names[prediction[0]]
+    def predict_species(sepal_length, sepal_width, petal_length, petal_width):
+    import numpy as np
+    features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    return model.predict(features)[0]
+
