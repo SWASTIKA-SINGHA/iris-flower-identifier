fea0417a665f235a2ffa10ce952c3b7f7ffb1687
SWASTIKA-SINGHA <swastikas857@gmail.com>
2025-07-21 12:34:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ sepal_width = st.slider('Sepal width', 2.0, 4.5)
 petal_length = st.slider('Petal length', 1.0, 7.0)
 petal_width = st.slider('Petal width', 0.1, 2.5)
 
-prediction = clf.predict([[sepal_length, sepal_width, petal_length, petal_width]])
-st.write(f"Predicted Iris type: {iris.target_names[prediction[0]]}")
+if st.button("predict"):
+  prediction=clf.predict([[sepal_length,sepal_width,petal_length,petal_width]])
+  st.success(f"predicted iris type:{iris.target_names[prediction[0]]}")
 
